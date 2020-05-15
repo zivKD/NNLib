6d6f7389ab93ada0c8d4345839415f3c34e4dd1e
ziv <ziv4545@gmail.com>
2020-05-15 11:38:30 +0300

    Created BaseDB class with all the needed methods so far

--- MyNNLib/DAL/BaseDB.py
+++ MyNNLib/DAL/BaseDB.py
@@ -1,28 +1,42 @@
 from abc import ABC, abstractmethod
 
 class BaseDB(ABC):
-
     @abstractmethod
-    def insertWeights(self, w):
-        pass
-
-
-    @abstractmethod
-    def insertBiases(self, b):
+    def insertWeights(self, w, layerId):
         pass
 
     @abstractmethod
-    def insertLearningRate(self, n):
+    def insertBiases(self, b, layerId):
         pass
 
     @abstractmethod
-    def insertSizeOfMiniBatches(self, m):
+    def insertLearningRate(self, n, layerId):
         pass
 
     @abstractmethod
-    def insertRegularizationTerm(self, lambada):
+    def insertSizeOfMiniBatches(self, m, layerId):
         pass
 
     @abstractmethod
-    def insertNumberOfEpoches(self, n):
+    def insertRegularizationTerm(self, lambada, layerId):
         pass
+
+    @abstractmethod
+    def insertNumberOfEpoches(self, num, layerId):
+        pass
+
+    @abstractmethod
+    def insertLocalReceptiveSize(self, size, layerId):
+        pass
+
+    @abstractmethod
+    def insertStride(self, stride, layerId):
+        pass
+
+    @abstractmethod
+    def getByLayerId(self, layerId):
+        pass
+
+
+
+
