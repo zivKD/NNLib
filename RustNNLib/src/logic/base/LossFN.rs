90d2a744f645a52ad56b5f5b569a88f1726d7f4c
ziv <ziv4545@gmail.com>
2021-08-17 23:57:04 +0300

    managed to order to mods and create quadratic loss

--- RustNNLib/src/logic/base/LossFN.rs
+++
@@ -1,4 +0,0 @@
-pub trait LossFN {
-    fn loss(&self, a: Vec<f64>, y: Vec<f64>) -> Vec<f64>;
-    fn derivative(&self, a: Vec<f64>, y: Vec<f64>, z: Vec<f64>) -> Vec<f64>;
-}
--- RustNNLib/src/main.rs
+++ RustNNLib/src/main.rs
@@ -1,70 +1,11 @@
-pub struct QuadraticLossFN {}
+mod logic;
+mod data;
 
-pub trait LossFN {
-    fn loss(&self, a: Vec<f64>, y: Vec<f64>) -> Vec<f64>;
-    fn derivative(&self, a: Vec<f64>, y: Vec<f64>, z: Vec<f64>) -> Vec<f64>;
-}
-
-mod math {
-    pub fn forLoop<F>(len: usize, func: F)
-        where F: FnMut(usize) -> () {
-        let mut i = 0;
-        while i < len {
-            func(i);
-            i = i + 1;
-        }
-    }
-
-    // fn vectorAction<F>(len: usize, func: F)
-    //     where F: Fn(Vec<f64>) -> Fn(usize) -> () {
-    //     let mut c = Vec::new();
-    //     forLoop(len, func(c));
-    //     return c;
-    // }
-
-    pub fn vectorSubraction(a: Vec<f64>, b: Vec<f64>) -> Vec<f64> {
-        let mut c : Vec<f64> = (0f64..a.len() as f64).collect();
-        forLoop(a.len(), |i: usize| {
-            c[i] = a[i] - b[i];
-        });
-
-        return c;
-    }
-
-    pub fn vectorPower(a: Vec<f64>, power: f64) -> Vec<f64> {
-        let mut c = Vec::new();
-        forLoop(a.len(), |i: usize| {
-            c[i] = a[i].powf(power);
-        });
-
-        return c;
-    }
-
-    pub fn vectorScalarMutliplication(a: Vec<f64>, scalar: f64) -> Vec<f64> {
-        let mut c = Vec::new();
-
-        forLoop(a.len(), |i: usize| {
-            c[i] = a[i] * scalar;
-        });
-
-        return c;
-    }
-}
-
-impl LossFN for QuadraticLossFN {
-    fn loss(&self, a: Vec<f64>, y: Vec<f64>) -> Vec<f64> {
-        let v1 = math::vectorSubraction(a, y);     
-        let v2 = math::vectorPower(v1, 2f64);     
-        return math::vectorScalarMutliplication(v2, 0.5f64);
-    }
-
-    fn derivative(&self, a: Vec<f64>, y: Vec<f64>, z: Vec<f64>) -> Vec<f64> {
-       return vec!(10f64, 10f64, 10f64);
-    }
-}
+use logic::base::LossFN as LossFN;
 
 fn main() {
-    let quatratic = QuadraticLossFN {};
-    let v = quatratic.loss(vec!(10f64, 10f64, 10f64), vec!(8f64, 8f64, 8f64));
-    println!("Initial vector: {:?}", v);
+    let a = vec!(4.0, 10.0, 10.0);
+    let b = vec!(8.0, 8.0, 8.0);
+    let quadratic = logic::loss_fns::QuadraticLossFN {};
+    println!("vector after qaudratic {:?}", quadratic.loss(&a, &b))
 }
